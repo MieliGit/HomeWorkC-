759aae1d1f8032cc6fe45b78f1692b2faa939576
Zakharov Daniil <zdan14@mail.ru>
2023-02-27 16:27:13 +0300

    Add Task_66

---
+++ Task_66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.Clear();
+ 
+int M = Input("Введите начальное число");
+int N = Input("Введите конечное число");
+
+Console.WriteLine(NumberListRec(M, N));
+
+int NumberListRec(int a, int b)
+{
+    if(b == a) return b;
+    else return b + NumberListRec(a, b - 1);
+}
+
+
+int Input(string message)
+{
+    Console.Write($"{message}: ");
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
